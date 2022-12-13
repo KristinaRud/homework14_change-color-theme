9330d07709970f66a0b7a4135717741a91541c42
@kristina.rud5 <kristina.rud5@gmail.com>
2022-12-13 19:54:58 +0200

    added event for document

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
+"use strick";
+
+document.addEventListener("DOMContentLoaded", function () {
+  if (localStorage.getItem("classTheme")) {
+    const theme =
+      localStorage.getItem("classTheme") === "dark" ? "dark" : "light";
+    document.body.classList.toggle(theme);
+    console.log("DOM fully loaded and parsed");
+  }
+});
+
 document.querySelector(".fa-adjust").addEventListener("click", (event) => {
-    event.preventDefault();
-    document.body.classList.toggle("dark");
+  event.preventDefault();
+  document.body.classList.toggle("dark");
+  const theme = document.body.classList.contains("dark") ? "dark" : "light";
+  localStorage.setItem("classTheme", theme);
 });
